0ed00337ca0f4be31fa2d1bc511e4ad4153b8194
Tiago Bankow <bkw.designer@gmail.com>
2024-10-07 15:02:59 +0000

    refactor: migrations + edit database schema + create `units_table`

--- infra/migrations/20241004184533_create-table-units.ts
+++ infra/migrations/20241004184533_create-table-units.ts
@@ -1,23 +1,18 @@
 import type { Knex } from "knex";
 
-const TABLE_NAME = "products";
+const TABLE_NAME = "units";
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable(TABLE_NAME, (table) => {
     table.increments("id", { primaryKey: true });
-    table.integer("user_id").unsigned();
-    table.integer("unit_id").unsigned();
     table.string("name", 128).unique().notNullable();
-    table.float("stock", 8.2).notNullable().defaultTo(0);
     table.string("description", 255).unique().notNullable();
+    table.string("ratio").notNullable().defaultTo("1:1");
 
     table.boolean("is_active").defaultTo(false);
     table.boolean("is_deleted").defaultTo(true);
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
-
-    table.foreign("user_id").references("id").inTable("users");
-    table.foreign("unit_id").references("id").inTable("units");
   });
 }
 
--- infra/migrations/20241004192003_create-table-product_histories.ts
+++ infra/migrations/20241004192003_create-table-product_histories.ts
@@ -1,4 +1,3 @@
-import { UNITS } from "../../constants/types";
 import type { Knex } from "knex";
 
 const TABLE_NAME = "product_histories";
@@ -7,27 +6,23 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable(TABLE_NAME, (table) => {
     table.increments("id", { primaryKey: true });
     table.integer("user_id").unsigned();
-    table.foreign("user_id").references("id").inTable("users");
     table.integer("product_id").unsigned();
-    table.foreign("product_id").references("id").inTable("products");
-
-    table
-      .enu("unit", UNITS, {
-        enumName: "units_type",
-        useNative: true,
-        existingType: true,
-      })
-      .unique()
-      .notNullable();
+    table.integer("unit_id").unsigned();
+    table.float("quantity", 8.2).notNullable().defaultTo(1);
+    table.string("ratio").notNullable().defaultTo("1:1");
     table
       .enu("type", ["OUTBOUND", "INBOUND"], {
-        enumName: "units_type",
+        enumName: "history_type",
         useNative: true,
       })
       .unique()
       .notNullable();
 
     table.timestamp("created_at").defaultTo(knex.fn.now());
+
+    table.foreign("unit_id").references("id").inTable("units");
+    table.foreign("user_id").references("id").inTable("users");
+    table.foreign("product_id").references("id").inTable("products");
   });
 }
 
