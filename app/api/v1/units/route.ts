4ed626e7840e572e1927e6c0e5cee28fd28129df
Tiago Bankow <tiago.bankow@gmail.com>
2024-10-23 22:58:00 -0400

    refactor: units route

--- app/api/v1/units/route.ts
+++ app/api/v1/units/route.ts
@@ -1,21 +1,23 @@
-import { createUnit, getAllUnits } from "@/models/unit";
+import UnitModel from "@/models/unit";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const page = Number(request.nextUrl.searchParams.get("page")) || 1;
-  const perPage = Number(request.nextUrl.searchParams.get("perPage")) || 10;
-  const name = request.nextUrl.searchParams.get("name");
+  const searchParams: any = Object.fromEntries(
+    new URLSearchParams(request.nextUrl.searchParams)
+  );
+  const Unit = new UnitModel();
 
-  const units = await getAllUnits({ page, perPage, name });
+  const units = await Unit.getAllUnits(searchParams);
 
   return Response.json(units);
 }
 
 export async function POST(request: Request) {
+  const Unit = new UnitModel();
   const data = await request.json();
 
   try {
-    const unit = await createUnit(data);
+    const unit = await Unit.createUnit(data);
 
     return Response.json(unit, { status: 201 });
   } catch (error) {
