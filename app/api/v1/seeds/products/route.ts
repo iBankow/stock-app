98c3f38a58096bf8840163cea4c07b6b6b0248e8
Tiago Bankow <tiago.bankow@gmail.com>
2024-11-09 13:42:32 -0400

    feat: create seeds route

--- app/api/v1/seeds/products/route.ts
+++ app/api/v1/seeds/products/route.ts
@@ -3,7 +3,7 @@ import { faker } from "@faker-js/faker";
 
 function createRandomProduct() {
   return {
-    name: faker.commerce.product() + " " + faker.commerce.productAdjective(),
+    name: faker.commerce.productName(),
     description: faker.commerce.productDescription(),
     stock: Number(faker.commerce.price({ min: 0, max: 350, dec: 0 })),
   };
@@ -25,7 +25,3 @@ export async function GET() {
 
   return Response.json({ ok: "ok" }, { status: 201 });
 }
-
-async function insertData() {}
-
-insertData();
---
+++ app/api/v1/seeds/units/route.ts
@@ -0,0 +1,28 @@
+import UnitModel from "@/models/unit";
+import { faker } from "@faker-js/faker";
+import { IUnit } from "knex/types/tables.js";
+
+function createRandomProduct(): Partial<IUnit> {
+  return {
+    name: faker.science.unit().name,
+    description: faker.commerce.productDescription(),
+    ratio: `1:${faker.number.int({ min: 1, max: 24 })}`,
+  };
+}
+
+export async function GET() {
+  const Unit = new UnitModel();
+
+  let count = 0;
+  while (count < 10) {
+    const product = createRandomProduct();
+    try {
+      await Unit.createUnit(product);
+      count++;
+    } catch {
+      continue;
+    }
+  }
+
+  return Response.json({ ok: "ok" }, { status: 201 });
+}
